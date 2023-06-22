98236a3d36bb3b70c7b397247c35e6982d4324b4
Edible <rwoznicki@edu.cdv.pl>
2023-06-22 03:12:42 +0200

    Na zajęcia z vfx

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform target; // The central point to rotate around
+    public float rotationSpeed = 5f; // Speed of rotation
+    public float cameraDistance = 20f;
+    public float cameraDownwardRotation = 27f;
+
+    private float _yaw = 0f;
+    private bool _isRotating = false;
+
+    void Update()
+    {
+        // Check if the middle mouse button is pressed down
+        if (Input.GetMouseButtonDown(2))
+        {
+            _isRotating = true;
+        }
+        // Check if the middle mouse button is released
+        else if (Input.GetMouseButtonUp(2))
+        {
+            _isRotating = false;
+        }
+
+
+        // Rotate the camera if the middle mouse button is pressed
+        if (_isRotating)
+        {
+            // Get the mouse input
+            float mouseX = Input.GetAxis("Mouse X");
+
+            // Calculate the rotation amount
+            _yaw += mouseX * rotationSpeed;
+
+            // Rotate the camera around the target
+            Quaternion rotation = Quaternion.Euler(cameraDownwardRotation, _yaw, 0f);
+            Vector3 newPosition = rotation * new Vector3(0f, 0f, cameraDistance * -1) + target.position;
+            transform.rotation = rotation;
+            transform.position = newPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Player movement speed
+    public float rotationSpeed = 180f; // Player rotation speed
+
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Get input for player movement
+        float forwardInput = Input.GetAxis("Vertical");
+        float rotationInput = Input.GetAxis("Horizontal");
+
+        // Move the player forward and backward
+        Vector3 movement = transform.forward * forwardInput * moveSpeed * Time.deltaTime;
+        characterController.Move(movement);
+
+        // Rotate the player
+        float rotationAmount = rotationInput * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotationAmount);
+    }
+}
